c92c49155c489ebe2b97978111c0af96ce40a591
na260 <na260git@gmail.com>
2021-05-02 14:47:12 +0000

    sh修正

--- 00_sh/01_revert.sh
+++ 00_sh/01_revert.sh
@@ -3,13 +3,14 @@
 ## apache conf
 # 下記追加
 
+sed -i -e "s|/home/site/site1|/home/site/site1/prj01/public|" /etc/apache2/sites-available/vhost1.conf
+
 sed -i -e "22i <Directory /home/site/site1/prj01/public>" /etc/apache2/sites-available/vhost1.conf
 sed -i -e "23i     AllowOverride All" /etc/apache2/sites-available/vhost1.conf
 sed -i -e "24i     Require all granted" /etc/apache2/sites-available/vhost1.conf
 sed -i -e "25i     DirectoryIndex index.php" /etc/apache2/sites-available/vhost1.conf
 sed -i -e "26i </Directory>" /etc/apache2/sites-available/vhost1.conf
 
-sed -i -e "s|/home/site/site1|/home/site/site1/prj01/public|" /etc/apache2/sites-available/vhost1.conf
 systemctl restart php7.4-fpm apache2
 
 # fuel migrate
