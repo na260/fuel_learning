4048a9c11b199fa4c2eb804456ceaa41f8fe18e8
na260 <na260git@gmail.com>
2021-05-02 14:30:04 +0000

    復帰用sh

---
+++ 00_sh/01_revert.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## apache conf
+# 下記追加
+
+sed -i -e "22i <Directory /home/site/site1/prj01/public>" /etc/apache2/sites-available/vhost1.conf
+sed -i -e "23i     AllowOverride All" /etc/apache2/sites-available/vhost1.conf
+sed -i -e "24i     Require all granted" /etc/apache2/sites-available/vhost1.conf
+sed -i -e "25i     DirectoryIndex index.php" /etc/apache2/sites-available/vhost1.conf
+sed -i -e "26i </Directory>" /etc/apache2/sites-available/vhost1.conf
+
+sed -i -e "s|/home/site/site1|/home/site/site1/prj01/public|" /etc/apache2/sites-available/vhost1.conf
+systemctl restart php7.4-fpm apache2
+
+# fuel migrate
+mv /home/site/site1/prj01/fuel/app/config/development/migrations.php /home/site/site1/prj01/fuel/app/config/development/migrations.php.bak
+cd /home/site/site1/prj01
+php oil refine migrate
